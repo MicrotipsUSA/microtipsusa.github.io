a7841490adc7b25dd223f2d4d9f683c7e69ab7c2
Jordan Ruhl <jruhl@microtipsusa.com>
2017-10-26 14:20:55 -0400

    working on contact form on index page

--- sendContactForm.php
+++ sendContactForm.php
@@ -1,11 +1,15 @@
 <?php
-if (isset($_POST['fullName'])) {
 $name = strip_tags($_POST['fullName']);
+$comp = strip_tags($_POST['companyName']);
+$location = strip_tags($_POST['companyLocation']);
 $email = strip_tags($_POST['contactEmail']);
+$phonenum = strip_tags($_POST['contactPhoneNumber']);
 $message = strip_tags($_POST['message']);
-echo "<strong>Name</strong>: ".$name."</br>";
-echo "<strong>Email</strong>: ".$email."</br>";
-echo "<strong>Message</strong>: ".$message."</br>";
-echo "<span class='label label-info'>Your feedback has been submitted with above details!</span>";
-}
+
+$formcontent = " From: $name \n $comp \n $location \n $email \n $phonenum \n $message \n";
+$recipient = "jordanruhl@gmail.com"
+$subject = "Modal contact test"
+$mailheader = "From: $email \r\n"
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank you!" . " - " . "<a href='http://www.facebook.com'>go to facebook</a>"
 ?>
