98d60979a70ed9d3b92cd60871300c213fa4dc4d
Jordan Ruhl <jruhl@microtipsusa.com>
2017-10-26 14:00:20 -0400

    working on contact form on index page

--- sendContactForm.php
+++ sendContactForm.php
@@ -1,10 +1,11 @@
 <?php
-
-if(isset($_POST[fullName]) && isset($_POST[companyName]) && isset($_POST[companyLocation]) && isset($_POST[contactEmail]) && isset($_POST[contactPhoneNumber]) && isset($_POST[message])){
-  mail('jordanruhl@gmail.com', 'all good', 'all good');
+if (isset($_POST['fullName'])) {
+$name = strip_tags($_POST['fullName']);
+$email = strip_tags($_POST['contactEmail']);
+$message = strip_tags($_POST['message']);
+echo "<strong>Name</strong>: ".$name."</br>";
+echo "<strong>Email</strong>: ".$email."</br>";
+echo "<strong>Message</strong>: ".$message."</br>";
+echo "<span class='label label-info'>Your feedback has been submitted with above details!</span>";
 }
-else {
-  mail('jordanruhl@gmail.com', 'ALL NOT GOOD', 'ALL NOT GOOD');
-}
-
 ?>
